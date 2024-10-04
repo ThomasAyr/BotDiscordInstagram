49f48474aa6a079bb56f3029a13e9fc6c554b666
Thomas Ayrivié <99038943+ThomasAyr@users.noreply.github.com>
2024-10-04 20:14:42 +0200

    Update bot.py
    Fix error secret
    Update on_ready function

--- bot.py
+++ bot.py
@@ -9,10 +9,8 @@ def get_latest_instagram_posts(username):
     """
     Renvoie un json des 10 derniers posts d'instagram de username
     """
-    # Créer une instance d'Instaloader
     L = instaloader.Instaloader()
 
-    # Charger le profil public Instagram
     profile = instaloader.Profile.from_username(L.context, username)
 
     # Récupérer les derniers posts
@@ -29,15 +27,13 @@ def get_latest_instagram_posts(username):
 
         if len(posts) >= 10:
             break
-
     return posts
 
 
-# Appliquer le patch pour permettre d'exécuter asyncio.run() dans Jupyter
 nest_asyncio.apply()
 
-TOKEN = ${{ secrets.TOCKEN_BOT_DISCORD_INSTAGRAM }}
-CHANNEL_ID = 1201173528221864116 
+TOKEN = os.getenv('TOCKEN_BOT_DISCORD_INSTAGRAM')
+
 
 class MyClient(discord.Client):
     async def on_ready(self):
@@ -55,46 +51,37 @@ intents = discord.Intents.default()
 intents.message_content = True
 client = MyClient(intents=intents)
 
-
 @client.event
 async def on_ready():
+    """
+    Fonction qui envoie un embed pour chaque post publié par cfa_ensup_lr dans les dernières 24 heures vers le salon CHANNEL_ID.
+    """
     print(f'Logged in as {client.user}')
-    
-    # Obtenir le canal Discord où envoyer les messages
+
+    CHANNEL_ID = 1201173528221864116 
     channel = client.get_channel(CHANNEL_ID)
+    current_time = datetime.utcnow()
 
-    while True:
-        # Obtenir l'heure actuelle en UTC
-        current_time = datetime.utcnow()  # Obtenir l'heure actuelle UTC
-        next_run_time = current_time.replace(minute=5, second=0, microsecond=0) + timedelta(hours=1)
+    # Récupérer les derniers posts Instagram
+    posts = get_latest_instagram_posts('cfa_ensup_lr')
 
-        print("Démarrage dans",(next_run_time - current_time).total_seconds())
+    # Envoyer les posts Instagram sur Discord
+    for post in posts:
+        if (current_time - post['date']).total_seconds() < 24 * 3600 and post["description"] is not None:
+            titre =  post['description'].split('\n')[0].upper().strip("[]")
 
-        # Attendre jusqu'au prochain lancement
-        await asyncio.sleep((next_run_time - current_time).total_seconds())
+            embed = discord.Embed(
+                title= titre,
+                description= post['description'][len(post['description'].split('\n')[0]):],
+                color=0x1E90FF,
+                url=post['urlpost']
+            )
+            embed.set_image(url=post['urlphoto'])
+            datet = post['date'].strftime("%d/%m/%Y %H:%M")
+            embed.set_footer(text=f"cfa_ensup_lr | instagram.com • {datet}")
 
-        print("Démarrage")
+            await channel.send(embed=embed)
+            
+    await client.close() 
 
-        # Récupérer les derniers posts Instagram
-        username = 'cfa_ensup_lr'
-        posts = get_latest_instagram_posts(username)
-
-        # Envoyer les posts Instagram sur Discord
-        for post in posts:
-            if (current_time - post['date']).total_seconds() < 3600 and post["description"] is not None:
-                titre =  post['description'].split('\n')[0].upper().strip("[]")
-
-                embed = discord.Embed(
-                    title= titre,
-                    description= post['description'][len(post['description'].split('\n')[0]):],
-                    color=0x1E90FF,
-                    url=post['urlpost']
-                )
-                embed.set_image(url=post['urlphoto'])
-                datet = post['date'].strftime("%d/%m/%Y %H:%M")
-                embed.set_footer(text=f"cfa_ensup_lr | instagram.com • {datet}")
-
-                await channel.send(embed=embed)
-
-# Lancer le bot Discord
 client.run(TOKEN)
